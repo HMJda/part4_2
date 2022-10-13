59a2a5cc9e9718aaa12aa68b8955e2ec503acb34
deucomputer <deucomputer@outlook.kr>
2022-10-13 09:57:32 +0900

    초기 커밋.

---
+++ ClickEvent/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Windows.Forms;
+class ClickEventApp : Form
+{
+    public ClickEventApp()
+    { // 생성자
+        this.Text = "ClickEventApp";
+        this.Click += new EventHandler(ClickEvent); //  등록
+    }
+    // ... 이벤트 처리기 ...
+    private void ClickEvent(object sender, EventArgs e)
+    {
+        MessageBox.Show(" sender = " + sender.GetType());
+    }
+    public static void Main()
+    {
+        Application.Run(new ClickEventApp());
+    }
+}
