64b7f9fe6038152e1aeba8c3c44408202531cd8c
deucomputer <hmj4669@gmail.com>
2022-10-13 10:24:33 +0900

    구조체와 클래스 복사의 차이점

---
+++ copyClassAndStruct/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+class Program
+{
+    class PointClass // nested class
+    {
+        public int x;
+        public int y;
+        public PointClass(int x, int y)
+        {
+            this.x = x;
+            this.y = y;
+        }
+    }
+    struct PointStruct //
+    {
+        public int x;
+        public int y;
+        public PointStruct(int x, int y)
+        {
+            this.x = x;
+            this.y = y;
+        }
+    }
+    static void Main(string[] args)
+    {
+        // 클래스 -> 얕은 복사 (참조)
+        PointClass pointClassA = new PointClass(10, 20);
+        PointClass pointClassB = pointClassA;
+        pointClassB.x = 100;
+        pointClassB.y = 200;
+        Console.WriteLine("pointClassA :" + pointClassA.x + "," + pointClassA.y);
+        Console.WriteLine("pointClassB :" + pointClassB.x + "," + pointClassB.y);
+        // 구조체 -> 깊은 복사 (값)
+        PointStruct pointStructA = new PointStruct(10, 20);
+        PointStruct pointStructB = pointStructA;
+        pointStructB.x = 100;
+        pointStructB.y = 200;
+        Console.WriteLine("pointStructA :" + pointStructA.x + "," + pointStructA.y);
+        Console.WriteLine("pointStructB :" + pointStructB.x + "," + pointStructB.y);
+    }
+}
